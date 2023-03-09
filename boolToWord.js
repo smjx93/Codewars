5411b5f6c7aab250aa87e6a3d85d904572aa2f5e
Sam Mikael Jones <110736332+smjx93@users.noreply.github.com>
2023-03-09 09:41:18 +0100

    Return "Yes" for true or "No" for false.
    // Complete the method that takes a boolean value and return
    // a "Yes" string for true, or a "No" string for false.

---
+++ boolToWord.js
@@ -0,0 +1,15 @@
+// Complete the method that takes a boolean value and return
+// a "Yes" string for true, or a "No" string for false.  
+
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+  }
+  
+  //This function takes a boolean argument bool 
+  //   and returns the string 'Yes' if bool is true, 
+  //   and 'No' if bool is false. 
+    
+  //   The expression 
+  //   bool ? 'Yes' : 'No' 
+  //   is called a ternary operator, 
+  //   which is a shorthand way to write an if-else statement.
