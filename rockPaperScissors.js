c2a775ab50fe2adb1c57c056af43b3caef99f8c5
Sam Mikael Jones <110736332+smjx93@users.noreply.github.com>
2023-03-16 11:15:59 +0100

    Return the winner between two values

---
+++ rockPaperScissors.js
@@ -0,0 +1,10 @@
+
+// return the winner of two values between p1 and p2
+
+const rps = (p1, p2) => {
+  if (p1 === p2) return 'Draw!';
+  if (p1 === 'rock' && p2 === 'scissors') return 'Player 1 won!';
+  if (p1 === 'scissors' && p2 === 'paper') return 'Player 1 won!';
+  if (p1 === 'paper' && p2 === 'rock') return 'Player 1 won!';
+  return 'Player 2 won!';
+};
