122451be4046a38f5f148600527aeec380b56d30
Sam Mikael Jones <110736332+smjx93@users.noreply.github.com>
2023-03-27 13:00:40 +0200

    Check if integer is either Even or Odd

---
+++ evenOrOdd.js
@@ -0,0 +1,11 @@
+/* Create a function that takes an integer as an argument and 
+returns "Even" for even numbers or "Odd" for odd numbers.*/
+
+const evenOrOdd = number => number % 2 === 0 ? "Even" : "Odd";
+
+
+/*When there's only one parameterthe parentheses can be omitted
+The ternary statement checks if the remainder of the number 
+divided by 2 is equal to 0 (i.e., the number is even), and 
+returns "Even". Otherwise, it returns "Odd" if the number is odd.
+*/
