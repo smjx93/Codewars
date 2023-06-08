c7c12a3406e5fb642fbf9a555b462cf84eca5271
Sam Mikael Jones <110736332+smjx93@users.noreply.github.com>
2023-06-08 12:57:13 +0200

    square an integer

---
+++ squareInt.js
@@ -0,0 +1,13 @@
+/* Now you have to write a function that takes an argument and returns the square of it. */
+
+
+function square(number) {
+    return number * number;
+  }
+
+console.log(square(2))
+console.log(square(3))
+console.log(square(4))
+
+
+
